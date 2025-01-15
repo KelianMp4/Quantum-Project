b83c6d332cf8cd799de5c3598ce418af68bff5df
Kélian Chapoutot <chapoutot.kelian@yahoo.com>
2025-01-15 01:25:42 +0100

    Fix + Update!

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Quantum project
-## [Project_1 :](project_1/main.py)
+
+## [Project_1 :](project_1/main.py) Simulating a quantum random number generator
+
+
+---
+
 ## [Project_2 :](project_2/main.py)
--- project_1/main.py
+++ project_1/main.py
@@ -4,33 +4,36 @@ from qiskit_aer import AerSimulator
 import os
 
 
-def random_number_quantum(long: int, iteration=0) -> str:
-    i: int = 0
-    number: str = ''
+def random_number_quantum(n_bits: int, n_execution: int = 0):
+    os.makedirs(name='Hist', exist_ok=True)
     simulator = AerSimulator()
+    n: int = 0
 
-    while i < long:
-        qc = QuantumCircuit(1)
-        qc.h(0)
-        qc.measure_all()
-        compiled_circuit = transpile(qc, simulator)
-        result = simulator.run(compiled_circuit, shots=1024).result()
-        counts = result.get_counts()
-        number += str(max(counts, key=counts.get))
-        i += 1
+    for _ in range(n_execution):
+        i: int = 0
+        number: str = ''
+        n += 1
 
-    plt.bar([0, 1], [number.count('0'), number.count('1')], color=['blue', 'red'])
-    plt.title(f'Histogram of quantum numbers: {number}')
-    plt.xlabel('quantum number')
-    plt.ylabel('count')
-    plt.savefig(f'Hist/quantum{iteration}.png')
-    plt.close()
+        while i < n_bits:
+            qc = QuantumCircuit(1)
+            qc.h(0)
+            qc.measure_all()
+            compiled_circuit = transpile(qc, simulator)
+            result = simulator.run(compiled_circuit, shots=1024).result()
+            counts = result.get_counts()
+            number += str(max(counts, key=counts.get))
+            i += 1
 
-    return (f"binary number : {number};\n"
-            f"base 10 number: {int(number, 2)}")
+        plt.bar([0, 1], [number.count('0'), number.count('1')], color=['blue', 'red'])
+        plt.title(f'Histogram of quantum numbers: {number} : {int(number, 2)}')
+        plt.xlabel('quantum number')
+        plt.ylabel('count')
+        plt.savefig(f'Hist/quantum{n}.png')
+        plt.close()
+
+        print(f"binary number : {number};\n"
+              f"base 10 number: {int(number, 2)}")
 
 
 if __name__ == '__main__':
-    os.makedirs('Hist', exist_ok=True)
-    for j in range(10):
-        print(random_number_quantum(10, j))
+    print(random_number_quantum(n_bits=10, n_execution=10))
